ed117a22abc107365d49ddf3a1ed9c0b2a059ada
mfghir <fatemeghafari77@gmail.com>
2023-12-01 00:54:26 -0800

    done navbar

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
-import { theme } from './theme'
+import { theme } from './theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
