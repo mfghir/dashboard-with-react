caf3bc26167b4a1fc97314d97179bfe9dfc06fb5
mfghir <fatemeghafari77@gmail.com>
2023-12-02 00:19:45 -0800

    add feed + sidebar content

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,30 @@
-// import { Box } from '@mui/material'
-
+import { Box, Stack, Skeleton } from "@mui/material";
+import  { useState } from "react";
 
 
 const Feed = () => {
-    return (
-        <div>Feed</div>
-    )
-}
+  const [loading, setLoading] = useState(true);
 
-export default Feed
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
+
+  return (
+    <Box flex={4} p={{ xs: 0, md: 2 }}>
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
+        </Stack>
+      ) : (
+        <>
+     ""
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default Feed;
